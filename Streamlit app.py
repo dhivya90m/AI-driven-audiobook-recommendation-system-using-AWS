33878be6b1cc3991a0bbf7b9736bbdbd077666f7
DhivMari <69674869+dhivya90m@users.noreply.github.com>
2025-04-23 12:09:52 -0700

    Create Streamlit app.py

---
+++ Streamlit app.py
@@ -0,0 +1,30 @@
+!pip install streamlit
+!pip install pyngrok
+!pip install requests
+
+%%writefile app.py
+import streamlit as st
+import requests
+
+# Replace with your API Gateway Invoke URL
+api_url = "https://fcx146y4kc.execute-api.us-east-1.amazonaws.com/prod/query"
+
+# Streamlit app UI
+st.title("Audiobook Recommendation")
+
+query = st.text_input("Enter your query:")
+if query:
+    # Sending the user input query to the API Gateway (Lambda function)
+    response = requests.post(api_url, json={"query": query})
+
+    # Check if the response is successful
+    if response.status_code == 200:
+        data = response.json()
+        recommendations_text = data.get("recommendations", "")
+    if recommendations_text:
+        st.write("Top 3 Recommended Books:")
+        st.markdown(recommendations_text)
+    else:
+        st.write("No recommendations found.")
+
+
